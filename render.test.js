cfedff8e0fec996d52ff78f5cca22b0a35327389
Nathan Stevens <mr.bloopbloop@gmail.com>
2020-09-02 04:40:11 -0500

    Added tests for render.list.
    Tests currently fail.
    There is no implementation for render.list yet.

--- render.test.js
+++ render.test.js
@@ -37,3 +37,17 @@ headers.forEach((n) => {
 		expect(res.write).toHaveBeenCalledWith("<h"+ n + ">test</h" + n + ">")
 	})
 })
+
+test("render.list Should send <ol> and <li> elements", () => {
+	var res = new ServerResponse()
+	var list = ['item 1', 'item 2', "item 3", "item 4"]
+	render.list(res, list)
+	expect(res.write).toHaveBeenCalledWith("<ol><li>item 1</li><li>item 2</li><li>item 3</li><li>item 4</li></ol>")
+})
+
+test("render.list Should send <ul> and <li> elements", () => {
+	var res = new ServerResponse()
+	var list = ['item 1', 'item 2', "item 3", "item 4"]
+	render.list(res, list, false)
+	expect(res.write).toHaveBeenCalledWith("<ul><li>item 1</li><li>item 2</li><li>item 3</li><li>item 4</li></ul>")
+})
