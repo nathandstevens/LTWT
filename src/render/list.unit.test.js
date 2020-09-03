fbc1dae0d45ceddd9c31476303ecd7803df5cfd6
Nathan Stevens <mr.bloopbloop@gmail.com>
2020-09-03 05:09:24 -0500

    Moved list.unit.test.js to src/render.

--- src/render/list.unit.test.js
+++ src/render/list.unit.test.js
@@ -1,6 +1,5 @@
-const render = require('./render.js')
-const { ServerResponse, IncomingMessage } = require('http')
-const fs = require('fs')
+const list = require('./list.js')
+const { ServerResponse} = require('http')
 jest.mock('http')
 
 
@@ -8,25 +7,19 @@ jest.mock('http')
 beforeEach(() => {
   // Clear all instances and calls to constructor and all methods:
   ServerResponse.mockClear()
-  IncomingMessage.mockClear()
 });
-
-
-
-
-
 
 
 test("render.list Should send <ol> and <li> elements", () => {
 	var res = new ServerResponse()
-	var list = ['item 1', 'item 2', "item 3", "item 4"]
-	render.list(res, list)
+	var items = ['item 1', 'item 2', "item 3", "item 4"]
+	list(res, items)
 	expect(res.write).toHaveBeenCalledWith("<ol><li>item 1</li><li>item 2</li><li>item 3</li><li>item 4</li></ol>")
 })
 
 test("render.list Should send <ul> and <li> elements", () => {
 	var res = new ServerResponse()
-	var list = ['item 1', 'item 2', "item 3", "item 4"]
-	render.list(res, list, false)
+	var items = ['item 1', 'item 2', "item 3", "item 4"]
+	list(res, itmes, false)
 	expect(res.write).toHaveBeenCalledWith("<ul><li>item 1</li><li>item 2</li><li>item 3</li><li>item 4</li></ul>")
 })
