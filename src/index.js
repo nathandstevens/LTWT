0b71cd7fb41b8340ab8dd0c086c4bcf760f91d31
Nathan Stevens <mr.bloopbloop@gmail.com>
2020-09-03 04:43:36 -0500

    Moved index.js to src/.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const server = require('./server.js')
 const routing = require('./route.js')
-const render = require('./render.js')
+const render = require('./render')
 
 module.exports = {
 	"start":server.start,
