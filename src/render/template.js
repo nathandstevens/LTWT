03e0ef23633a41a5cd177c80d5126abb173d939e
Nathan Stevens <mr.bloopbloop@gmail.com>
2020-09-04 04:48:58 -0500

    Moved render.template to srce/render.

---
+++ src/render/template.js
@@ -0,0 +1,55 @@
+const {access, createReadStream} = require('fs')
+
+const templatePath = process.env.LTWTtemplatepath || "./resources/templates/"
+
+// Template rendering stream object
+function TemplateRenderer(placeholders, options) {
+	// allow use without new
+	if (!(this instanceof TemplateRenderer)) {
+		return new TemplateRenderer(placeholders, options)
+	}
+
+	// init Transform
+	Transform.call(this, options)
+	this.placeholders = placeholders
+}
+util.inherits(TemplateRenderer, Transform)
+
+TemplateRenderer.prototype._transform = function(chunk, enc, cb) {
+	let renderedTemplate = chunk.toString()
+	
+	Object.entries(this.placeholders).forEach((entry) => {
+		let [key, value] = entry
+		let search = RegExp("{{ " + key + " }}", "g" )
+		renderedTemplate = renderedTemplate.replace(search, value)
+	})
+	
+	this.push(renderedTemplate)
+	cb()
+}
+
+
+
+// Render template and replace placeholders
+const template = (filepath, res, placeholders={}, httpcode=200) => {
+	let readStream = fs.createReadStream(templatePath + filepath, {encoding:'utf8'})
+	let renderer = TemplateRenderer(placeholders,{})
+	
+	readStream.on('open', () => {			// readStream open event
+		readStream.pipe(renderer)
+		res.writeHead(httpcode, {'Content-Type': 'text/html'})
+		renderer.pipe(res)
+	})
+	
+	readStream.on('error', (err) => {		// readStream error event
+		error(500, filepath, res, err)
+
+	})
+	
+	renderer.on('error', (err) => {			// renderer error event
+		error(500, filepath, res, err)
+	})
+}
+
+
+module.exports = template
