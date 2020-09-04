5450bb6f84283ffee4d78a5bec30bd66f96fe659
Nathan Stevens <mr.bloopbloop@gmail.com>
2020-09-04 04:43:21 -0500

    Moved render.content to src/render.

--- render.js
+++ render.js
@@ -39,30 +39,6 @@ TemplateRenderer.prototype._transform = function(chunk, enc, cb) {
 
 
 
-// Element Renderers
-// Paragraph
-
-
-
-
-// Render arbitrary content
-const content = (filepath, contentType, res) => {
-	let readStream = fs.createReadStream(staticPath + filepath)
-	
-	readStream.on('open', () => {			// readStream open event
-		res.writeHead(200, {'Content-Type': contentType})
-		readStream.pipe(res)
-		console.log(200 + ":\t" + filepath)
-	})
-	
-	readStream.on('error', (err) => {		// readStream error event
-		fs.access(staticPath + filepath, fs.constants.F_OK, (err) => {
-			error(err ? 404 : 500, filepath, res, err ? '' : err)
-		})
-	})
-}
-
-
 // Render a static html file
 const page = (filepath, res) => {
 	let readStream = fs.createReadStream(staticPath + filepath)
---
+++ src/render/content.js
@@ -0,0 +1,20 @@
+const {access, createReadStream} = require('fs')
+
+// Render arbitrary content
+const content = (filepath, contentType, res) => {
+	let readStream = fs.createReadStream(staticPath + filepath)
+	
+	readStream.on('open', () => {			// readStream open event
+		res.writeHead(200, {'Content-Type': contentType})
+		readStream.pipe(res)
+		console.log(200 + ":\t" + filepath)
+	})
+	
+	readStream.on('error', (err) => {		// readStream error event
+		fs.access(staticPath + filepath, fs.constants.F_OK, (err) => {
+			error(err ? 404 : 500, filepath, res, err ? '' : err)
+		})
+	})
+}
+
+module.exports = content
