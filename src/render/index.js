b1b87903c8942d629494767d4c57b81a56215ded
Nathan Stevens <mr.bloopbloop@gmail.com>
2020-09-06 18:15:39 -0500

    Added index for render.

---
+++ src/render/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+	"page":require('./page.js'),
+	"content":require('./content.js'),
+	"template":require('./template.js'),
+	"error":require('./error.js'),
+	"para":require('./para.js'),
+	"head":require('./head.js'),
+	"list":require('./list.js')
+}
